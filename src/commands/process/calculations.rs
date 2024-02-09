754620dc811f92b1a629a2eeabd061fc1c7fe308
Gabriel Messias <gmessiasp@outlook.com>
2024-02-09 15:01:52 -0300

    Calculations tests.

--- src/commands/process/calculations.rs
+++ src/commands/process/calculations.rs
@@ -32,3 +32,68 @@ pub fn convert_minutes_to_hours(hours: &mut u8, minutes: &mut u8) {
         *minutes %= 60;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::models::time_record::TimeRecord;
+    use std::collections::HashMap;
+    use chrono::NaiveDate;
+
+    #[test]
+    fn test_calculate_total_hours_per_date() {
+        let mut records = HashMap::new();
+        let date = NaiveDate::from_ymd_opt(2022, 7, 8);
+        match date {
+            Some(d) => {
+                let record = TimeRecord {
+                    id: "123".to_string(),
+                    date: d,
+                    hours: 8,
+                    minutes: 70,
+                };
+
+                calculate_total_hours_per_date("2022-07-08".to_string(), &mut records, &record);
+
+                assert_eq!(records.get("2022-07-08"), Some(&(9, 10)));
+            },
+            None => {
+                panic!("Invalid date");
+            }
+        }
+    }
+
+    #[test]
+    fn test_calculate_total_hours_per_id() {
+        let mut records = HashMap::new();
+        let date = NaiveDate::from_ymd_opt(2022, 7, 8);
+        match date {
+            Some(d) => {
+                let record = TimeRecord {
+                    id: "123".to_string(),
+                    date: d,
+                    hours: 8,
+                    minutes: 70,
+                };
+
+                calculate_total_hours_per_id(&mut records, &record);
+
+                assert_eq!(records.get("123"), Some(&(9, 10)));
+            },
+            None => {
+                panic!("Invalid date");
+            }
+        }
+    }
+
+    #[test]
+    fn test_convert_minutes_to_hours() {
+        let mut hours = 1;
+        let mut minutes = 70;
+
+        convert_minutes_to_hours(&mut hours, &mut minutes);
+
+        assert_eq!(hours, 2);
+        assert_eq!(minutes, 10);
+    }
+}
